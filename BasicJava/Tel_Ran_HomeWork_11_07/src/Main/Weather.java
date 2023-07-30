79c7464df74951499ea206ca034d61c94b083ffc
MaksimLazuta <137941276+MaksimLazuta@users.noreply.github.com>
2023-07-30 22:42:27 +0300

    Delete BasicJava/Tel_Ran_HomeWork_11_07/src directory

--- BasicJava/Tel_Ran_HomeWork_11_07/src/Main/Weather.java
+++
@@ -1,8 +0,0 @@
-package Main;
-
-public class Weather {
-    public String nameOfTheDay = "Понедельник ";
-    public int temperature = 28;
-    public String howItFeel = "Прекрасная погода для прогулок";
-
-}
--- BasicJava/Tel_Ran_HomeWork_11_07/src/TextMessage.java
+++
@@ -1,7 +0,0 @@
-public class TextMessage {
-    public String answer = "";
-
-    public TextMessage(String answer) {
-        this.answer = answer.replace("fuck", "f**k").replace("bullshit", "b******t");
-    }
-}

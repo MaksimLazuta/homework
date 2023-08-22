2d90c18fa99eabd89b7f1d9e790bab51b8901cf2
MaksimLazuta <137941276+MaksimLazuta@users.noreply.github.com>
2023-08-22 20:32:51 +0300

    Privet Philipp

---
+++ BasicJava/BasicJavHW_07_08/src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public static int add(int a, int b) {
+
+        return a + b;
+    }
+    public static int substract(int a, int b) {
+
+        return a - b;
+    }
+
+    public static int multyply(int a, int b) {
+        return a * b;
+    }
+    public static int divide(int a, int b) {
+
+        return a / b;
+    }
+}
